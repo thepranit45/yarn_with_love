7cd3b5e4e7ea936cafb8e92e3b3a6a7b4837d25b
Pranit <thepranit45@gmail.com>
2025-12-19 06:59:25 +0530

    Fix logout issue: Use allauth account_logout and fix button styling

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,4 @@ urlpatterns = [
     path('login/customer/', auth_views.LoginView.as_view(template_name='users/login_customer.html'), name='login_customer'),
     path('login/artist/', views.login_artist, name='login_artist'),
     path('login/admin/', views.login_admin, name='login_admin'),
-    
-    # Logout
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
